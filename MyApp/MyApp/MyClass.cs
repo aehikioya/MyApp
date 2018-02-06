79c3e5538e66e27cb8d3d1d94fdbca8712e68133
Eric Lira <eric@dbcorp.com.br>
2018-02-06 14:10:19 -0200

    Add project files.

---
+++ MyApp/MyApp/MyClass.cs
@@ -0,0 +1,26 @@
+﻿namespace MyApp
+{
+	public class MyClass : BaseClass
+	{
+		private string name;
+		private bool isSelected;
+
+		public string Name
+		{
+			set { SetProperty(ref name, value); }
+			get { return name; }
+		}
+
+		public bool IsSelected
+		{
+			set { SetProperty(ref isSelected, value); }
+			get { return isSelected; }
+		}
+
+		public MyClass(string name, bool isSelected)
+		{
+			Name = name;
+			IsSelected = isSelected;
+		}
+	}
+}
---
+++ MyApp/MyApp/MyViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MyApp
+{
+	public class MyViewModel : BaseClass
+    {
+		private ObservableCollection<MyClass> myList;
+		private string selected;
+
+		public ObservableCollection<MyClass> MyList
+		{
+			set { SetProperty(ref myList, value); }
+			get { return myList; }
+		}
+
+		public string Selected
+		{
+			set { SetProperty(ref selected, value); }
+			get { return selected; }
+		}
+
+		public MyViewModel()
+		{
+			MyList = new ObservableCollection<MyClass>()
+			{
+				AddNew("John", false),
+				AddNew("Smith", false),
+				AddNew("Sara", false),
+			};
+		}
+
+		private MyClass AddNew(string name, bool isSelected)
+		{
+			var myClass = new MyClass(name, isSelected);
+			myClass.PropertyChanged += MyClass_PropertyChanged;
+			return myClass;
+		}
+
+		private void MyClass_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (sender is MyClass myClass)
+			{
+				Selected = $"{myClass.Name} is {myClass.IsSelected}";
+			}
+		}
+		
+	}
+}
